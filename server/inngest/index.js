a53d13674be9e371d7fe51d2f158df7e5ff10bfc
deepesh <deepesh2596@gmail.com>
2025-07-17 20:22:53 +0530

    updated index.js

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -7,7 +7,8 @@ import sendEmail from "../configs/nodeMailer.js";
 // Create a client to send and receive events
 export const inngest = new Inngest({
     id: "movie-ticket-booking",
-    eventKey: process.env.INNGEST_EVENT_KEY,
+    eventKey: process.env.INNGEST_EVENT_API_KEY,
+    signingKey: process.env.INNGEST_SIGNING_KEY,
 });
 
 
